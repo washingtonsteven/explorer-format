4795a1bf95d727d75856947e0d12332e92cd6a18
Steve Washington <washington.steven@gmail.com>
2022-09-06 01:25:15 -0400

    new node format

--- src/CanvasMap.ts
+++ src/CanvasMap.ts
@@ -1,7 +1,27 @@
 import { fabric } from "fabric";
+import nodeTest from "node:test";
 
 // TODO: maps that are larger than the canvas, allow for zoom, pan, etc.
 // See: http://fabricjs.com/fabric-intro-part-5#pan_zoom
+
+const NODE_BORDER_TABLE = [
+	"", // 0
+	"t", // 1
+	"r", // 2
+	"b", // 3
+	"l", // 4
+	"tr", // 5
+	"rb", // 6
+	"bl", // 7
+	"lt", // 8
+	"tb", // 9
+	"lr", // a
+	"rtb", // b
+	"trb", // c
+	"rbl", // d
+	"blt", // e
+	"tblr", // f
+];
 
 class CanvasMap {
 	canvasElem: HTMLCanvasElement;
@@ -27,116 +47,121 @@ class CanvasMap {
 			gridHeight: 20,
 			fillColor: "#ffd700",
 			borderColor: "#ccc",
-			nodes: [
-				{
-					gridX: 1,
-					gridY: 1,
-					borders: ["top", "left", "bottom"],
-					fill: true,
-					highlight: false,
-				},
-				{
-					gridX: 2,
-					gridY: 1,
-					borders: ["top", "bottom"],
-					fill: true,
-					highlight: false,
-				},
-				{
-					gridX: 3,
-					gridY: 1,
-					borders: ["top", "right"],
-					fill: true,
-					highlight: true,
-				},
-				{
-					gridX: 3,
-					gridY: 2,
-					borders: ["left", "bottom", "right"],
-					fill: true,
-					highlight: false,
-				},
-			],
+			nodes: `
+				8111115xxx
+				400003015x
+				40036x402x
+				736xxx733c
+			`,
+			highlight: {
+				x: 3,
+				y: 1,
+			},
 		};
 
-		// nodes
-		mapData.nodes.forEach((node) => {
-			const xCoord = node.gridX * mapData.nodeWidth;
-			const yCoord = node.gridY * mapData.nodeHeight;
+		mapData.nodes
+			.trim()
+			.split("\n")
+			.forEach((line, row) => {
+				line.trim()
+					.split("")
+					.forEach((node, col) => {
+						if (node === "x") {
+							return;
+						}
 
-			const nodeRect = new fabric.Rect({
-				left: xCoord,
-				top: yCoord,
-				fill: node.fill ? mapData.fillColor : "#00000000",
-				width: mapData.nodeWidth,
-				height: mapData.nodeHeight,
-				hasControls: false,
-			});
+						const xCoord = col * mapData.nodeWidth;
+						const yCoord = row * mapData.nodeHeight;
+						const r = new fabric.Rect({
+							left: xCoord,
+							top: yCoord,
+							fill: mapData.fillColor,
+							hasControls: false,
+							width: mapData.nodeWidth,
+							height: mapData.nodeHeight,
+						});
 
-			this.canvas.add(nodeRect);
+						this.canvas.add(r);
 
-			if (node.highlight) {
-				const highlightRect = new fabric.Rect({
-					left: xCoord,
-					top: yCoord,
-					fill: "#fff",
-					width: mapData.nodeWidth,
-					height: mapData.nodeHeight,
-					hasControls: false,
-					opacity: 0,
-				});
-				this.canvas.add(highlightRect);
+						if (
+							mapData.highlight &&
+							mapData.highlight.x === col &&
+							mapData.highlight.y === row
+						) {
+							const h = new fabric.Rect({
+								left: xCoord,
+								top: yCoord,
+								fill: "#fff",
+								width: mapData.nodeWidth,
+								height: mapData.nodeHeight,
+								hasControls: false,
+								opacity: 0,
+							});
+							this.canvas.add(h);
 
-				const animateOff = () => {
-					highlightRect.animate("opacity", 0, {
-						duration: 250,
-						onComplete: () => setTimeout(animateOn, 500),
-						onChange: this.canvas.renderAll.bind(this.canvas),
+							const animateOn = () => {
+								h.animate("opacity", 1, {
+									duration: 250,
+									onComplete: animateOff,
+									onChange: this.canvas.renderAll.bind(
+										this.canvas
+									),
+								});
+							};
+
+							const animateOff = () => {
+								h.animate("opacity", 0, {
+									duration: 250,
+									onComplete: () =>
+										setTimeout(animateOn, 500),
+									onChange: this.canvas.renderAll.bind(
+										this.canvas
+									),
+								});
+							};
+
+							animateOn();
+						}
+
+						const borders = NODE_BORDER_TABLE[parseInt(node, 16)];
+
+						if (borders) {
+							borders.split("").forEach((borderDir) => {
+								// top
+								let startX = xCoord;
+								let startY = yCoord;
+								let endX = xCoord + mapData.nodeWidth;
+								let endY = startY;
+
+								if (borderDir === "b") {
+									startY = yCoord + mapData.nodeHeight;
+									endY = startY;
+								}
+								if (borderDir === "r") {
+									startX = xCoord + mapData.nodeWidth;
+									endX = startX;
+									endY = yCoord + mapData.nodeHeight;
+								}
+								if (borderDir === "l") {
+									endX = startX;
+									endY = yCoord + mapData.nodeHeight;
+								}
+
+								const l = new fabric.Line(
+									[startX, startY, endX, endY],
+									{
+										stroke: "#333",
+										strokeWidth: 2,
+										hasControls: false,
+										strokeLineJoin: "round",
+									}
+								);
+
+								this.canvas.add(l);
+							});
+						}
 					});
-				};
-				const animateOn = () => {
-					highlightRect.animate("opacity", 1, {
-						duration: 250,
-						onComplete: animateOff,
-						onChange: this.canvas.renderAll.bind(this.canvas),
-					});
-				};
-				animateOn();
-			}
-
-			node.borders.forEach((borderDir) => {
-				// Default, top border
-				let startX = xCoord;
-				let startY = yCoord;
-				let endX = xCoord + mapData.nodeWidth;
-				let endY = startY;
-
-				// left
-				if (borderDir === "left") {
-					endX = startX;
-					endY = startY + mapData.nodeHeight;
-				}
-
-				if (borderDir === "bottom") {
-					startY = yCoord + mapData.nodeHeight;
-					endY = startY;
-				}
-
-				if (borderDir === "right") {
-					startX = xCoord + mapData.nodeWidth;
-					endX = startX;
-					endY = yCoord + mapData.nodeHeight;
-				}
-
-				const l = new fabric.Line([startX, startY, endX, endY], {
-					stroke: "#333",
-					strokeWidth: 2,
-					hasControls: false,
-				});
-
-				this.canvas.add(l);
 			});
-		});
 
 		// grid lines
 		let c = 0;
